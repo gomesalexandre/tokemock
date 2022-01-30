981cbe46d2b4ab87b49b0873364009b621d1c1fa
Alexandre Gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-01-30 12:13:01 +0300

    add hardhat-deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-web3");
+require("hardhat-deploy");
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
